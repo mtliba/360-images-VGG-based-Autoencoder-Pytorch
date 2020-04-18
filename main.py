c0bf8bd9d205f40cf0cbeeb13849cb8bedadf754
Marouane Tliba <61783328+mtliba@users.noreply.github.com>
2020-04-18 08:02:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
         transforms.Rescale(576,288),
         transforms.ToTensor(),
         transforms.Normalize(mean = [0.485, 0.456, 0.406],
-                            std = [0.229, 0.224, 0.225]))
+                            std = [0.229, 0.224, 0.225])
     ]))
     train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
